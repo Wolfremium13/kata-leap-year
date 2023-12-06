54c1a3cca5a6f2c49d0d6523795c714515437268
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-06 18:55:25 +0000

    ✅ be a leap year when is an atypical special year

--- src/main/kotlin/Year.kt
+++ src/main/kotlin/Year.kt
@@ -3,6 +3,9 @@ class Year {
         fun isLeapYear(year: Int): Boolean {
             if (year % 4 == 0) {
                 if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        return true
+                    }
                     return false
                 }
                 return true
